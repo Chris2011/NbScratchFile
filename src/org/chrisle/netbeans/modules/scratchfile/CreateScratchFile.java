8c41423b2f76c4fa510b55e4ca259db1094a9747
Chris <chrl@spreadshirt.net>
2015-05-11 16:27:09 +0200

    Tried extends the DataEditorSupport class to override saveDocument.

--- src/org/chrisle/netbeans/modules/scratchfile/CreateScratchFile.java
+++ src/org/chrisle/netbeans/modules/scratchfile/CreateScratchFile.java
@@ -3,16 +3,18 @@ package org.chrisle.netbeans.modules.scratchfile;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import javax.swing.JDialog;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionReferences;
 import org.openide.awt.ActionRegistration;
-import org.openide.cookies.OpenCookie;
+import org.openide.cookies.*;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileSystem;
 import org.openide.filesystems.FileUtil;
 import org.openide.loaders.DataObject;
 import org.openide.loaders.DataObjectNotFoundException;
+import org.openide.text.DataEditorSupport;
 import org.openide.util.Exceptions;
 import org.openide.util.NbBundle.Messages;
 
@@ -29,7 +31,15 @@ import org.openide.util.NbBundle.Messages;
     @ActionReference(path = "Shortcuts", name = "DOS-N")
 })
 @Messages("CTL_CreateScratchFile=New Scratch File...")
-public final class CreateScratchFile implements ActionListener {
+public final class CreateScratchFile extends DataEditorSupport implements ActionListener, EditorCookie, EditCookie, PrintCookie, CloseCookie {
+
+    public CreateScratchFile() {
+        super(null, null);
+    }
+    
+    public CreateScratchFile(DataObject obj, Env env) {
+        super(obj, env);
+    }
     @Override
     public void actionPerformed(ActionEvent e) {
         try {
@@ -43,5 +53,23 @@ public final class CreateScratchFile implements ActionListener {
         } catch (IOException ex) {
             Exceptions.printStackTrace(ex);
         }
+    }
+
+    @Override
+    public void saveDocument() throws IOException {
+        JDialog test = new JDialog();
+        test.setVisible(true);
+        
+        super.saveDocument(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean close() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void edit() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
