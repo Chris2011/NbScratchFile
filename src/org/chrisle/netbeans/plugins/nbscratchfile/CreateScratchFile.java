3962e7cf34aae09d9a8aa0a04c8e5923e60bffc3
Chris2011 <Christian.Lenz@gmx.net>
2016-01-04 00:05:13 +0100

    Refactored code.

--- src/org/chrisle/netbeans/plugins/nbscratchfile/CreateScratchFile.java
+++ src/org/chrisle/netbeans/plugins/nbscratchfile/CreateScratchFile.java
@@ -3,17 +3,16 @@ package org.chrisle.netbeans.plugins.nbscratchfile;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
-import javax.swing.JDialog;
-import javax.swing.JLabel;
-import org.netbeans.spi.actions.AbstractSavable;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.netbeans.api.actions.Openable;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionReferences;
 import org.openide.awt.ActionRegistration;
-import org.openide.cookies.OpenCookie;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileSystem;
 import org.openide.filesystems.FileUtil;
+import org.openide.loaders.DataFolder;
 import org.openide.loaders.DataObject;
 import org.openide.loaders.DataObjectNotFoundException;
 import org.openide.util.Exceptions;
@@ -32,23 +31,16 @@ import org.openide.util.NbBundle.Messages;
     @ActionReference(path = "Shortcuts", name = "DOS-N")
 })
 @Messages("CTL_CreateScratchFile=New Scratch File...")
-public final class CreateScratchFile extends AbstractSavable implements ActionListener {
-    private DataObject _data;
-    private final Object obj;
-
-    public CreateScratchFile(Object obj) {
-        this.obj = obj;
-        register();
-    }
+public final class CreateScratchFile implements ActionListener {
+    private static final AtomicInteger _atomInt = new AtomicInteger(0);
 
     @Override
     public void actionPerformed(ActionEvent e) {
         try {
-            FileSystem fs = FileUtil.createMemoryFileSystem();
-            FileObject fob = fs.getRoot().createData("Untitled");
-            this._data = DataObject.find(fob);
-            OpenCookie cookie = (OpenCookie) this._data.getLookup().lookup(OpenCookie.class);
-            cookie.open();
+            DataObject gdo = getDataObject();
+            Openable openable = gdo.getLookup().lookup(Openable.class);
+
+            openable.open();
         } catch (DataObjectNotFoundException ex) {
             Exceptions.printStackTrace(ex);
         } catch (IOException ex) {
@@ -56,32 +48,26 @@ public final class CreateScratchFile extends AbstractSavable implements ActionLi
         }
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (o instanceof CreateScratchFile) {
-            return ((CreateScratchFile)o).obj.equals(obj);
-        }
-        return false;
+    protected DataObject getDataObject() throws DataObjectNotFoundException, IOException {
+        String templateName = getTemplate();
+
+        FileObject fo = FileUtil.getConfigRoot().getFileObject(templateName);
+        DataObject template = DataObject.find(fo);
+
+        FileSystem memFS = FileUtil.createMemoryFileSystem();
+        FileObject root = memFS.getRoot();
+
+        DataFolder dataFolder = DataFolder.findFolder(root);
+        DataObject gdo = template.createFromTemplate(dataFolder, "New" + getNextCount());
+
+        return gdo;
     }
 
-    @Override
-    public int hashCode() {
-        return this.obj.hashCode();
+    protected String getTemplate() {
+        return "Templates/Other/file";
     }
 
-    @Override
-    protected void handleSave() {
-        JDialog test = new JDialog();
-        JLabel ll = new JLabel();
-
-        ll.setText("Es wird gespeichert.");
-        test.add(ll);
-        test.setSize(100, 100);
-        test.setVisible(true);
-    }
-
-    @Override
-    protected String findDisplayName() {
-        return "My name is " + obj.toString(); // get display name somehow
+    private static int getNextCount() {
+        return _atomInt.incrementAndGet();
     }
 }
